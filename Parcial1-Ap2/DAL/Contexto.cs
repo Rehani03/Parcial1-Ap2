7b1eebb081c6600057a62a799b69ab8dd27bc068
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-06-07 10:30:43 -0400

    Cambios..

---
+++ Parcial1-Ap2/DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1_Ap2.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=DATA\PrimerParcial.db");
+        }
+    }
+}
