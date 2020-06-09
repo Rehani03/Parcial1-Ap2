4361b066e5328d31e42b0133942e80828a6d58e3
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-06-09 08:37:26 -0400

    UnitTest creado..

---
+++ Parcial1-Ap2Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Parcial1_Ap2Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
