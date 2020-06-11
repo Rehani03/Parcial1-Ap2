c98a7357b3869d9ccd661c9ac8e45079a26554e4
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-06-10 20:35:41 -0400

    BLL creada..

--- Parcial1-Ap2/BLL/ProductoBLL.cs
+++ Parcial1-Ap2/BLL/ProductoBLL.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_Ap2.DAL;
+using Parcial1_Ap2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,130 @@ namespace Parcial1_Ap2.BLL
 {
     public class ProductoBLL
     {
+        public static bool Guardar(Producto producto)
+        {
+            if (!Existe(producto.productoId))//si no existe insertamos
+
+                return Insertar(producto);
+            else
+                return Modificar(producto);
+
+        }
+        private static bool Insertar(Producto producto)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Productos.Add(producto);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        private static bool Modificar(Producto producto)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+
+                contexto.Entry(producto).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var auxProducto = contexto.Productos.Find(id);
+                if (auxProducto != null)
+                {
+                    contexto.Productos.Remove(auxProducto);//remueve la entidad
+                    paso = contexto.SaveChanges() > 0;
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static Producto Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Producto producto;
+
+            try
+            {
+                producto = contexto.Productos.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return producto;
+
+        }
+
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Productos.Any(p=>p.productoId == id);
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+
+        }
     }
 }
